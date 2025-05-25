ba3fe46df06cdcb827ca82c4e45fdcae1897c4ba
Nurislam-React-Developer <98372q@gmailc.com>
2025-05-25 14:25:30 +0600

    feat: implement counter functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const [start, setStart] = useState(false)
+
+  useEffect(() => {
+    let time;
+
+    if(start)  {
+      time = setInterval(() => {
+        setCount((prev) => prev +1)
+      }, 1000)
+    }
+
+    return () => {
+      if(time) {
+        clearInterval(time)
+      }
+    }
+
+  }, [start])
   return (
-    <div>App</div>
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setStart((s) => !s)}>
+        {start ? 'Stop' : 'Start'}
+      </button>
+    </div>
   )
 }
 
